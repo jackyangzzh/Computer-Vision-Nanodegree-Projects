5a16487a1b2993d38eb8de1fef8ab0df7031d367
Jack Yang <jackyangzzh@gmail.com>
2020-12-30 19:51:54 -0600

    Save progress on getting the notebook to work

--- P1_Facial_Keypoints-master/models.py
+++ P1_Facial_Keypoints-master/models.py
@@ -1,7 +1,6 @@
 ## TODO: define the convolutional neural network architecture
 
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
 # can use the below import should you choose to initialize the weights of your Net
@@ -24,14 +23,29 @@ class Net(nn.Module):
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.conv3 = nn.Conv2d(64, 128, 5)
+        self.pool = nn.MaxPool2d(2,2)
         
+        self.fc1 = nn.Linear(128*24*24,1024)
+        self.fc1_drop = nn.Dropout(p=0.4)
+        self.fc2 = nn.Linear(1024,136)
 
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
         ## x = self.pool(F.relu(self.conv1(x)))
+
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.pool(F.relu(self.conv3(x)))
         
+        x = x.view(x.size(0),-1)
+        
+        x = F.relu(self.fc1(x))
+        x = self.fc1_drop(x)
+        x = self.fc2(x)
         
         # a modified x, having gone through all the layers of your model, should be returned
         return x
