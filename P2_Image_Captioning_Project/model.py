4cb9e1b0c7fca99a2d4daef1debcf259e076675e
Jack Yang <jackyangzzh@gmail.com>
2021-01-21 08:34:51 -0600

    Update training predictions

--- P2_Image_Captioning_Project/model.py
+++ P2_Image_Captioning_Project/model.py
@@ -23,11 +23,29 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, 0, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
     
     def forward(self, features, captions):
-        pass
+        captions = self.embedding(captions[:,:-1])
+        inputs = torch.cat((features.unsqueeze(1), captions), 1)
+        outputs,(h_n, c_n) = self.lstm(inputs)
+        outputs = self.linear(outputs)
+        return outputs
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-        pass
+        predicted = []
+        for i in range(max_len):
+            # LSTM 
+            hidden, states = self.lstm(inputs, states)
+            # Linear
+            output = self.linear(hidden.squeeze(1))
+            # Max
+            word = output.max(1)[1]
+            predicted.append(word.item())
+            inputs = self.embedding(word)
+            inputs = inputs.unsqueeze(1)
+        return predicted
